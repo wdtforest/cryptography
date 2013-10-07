b524afd07253c26823cb2dc3fc773a79c85d0632
luciguy <gaigai508@gmail.com>
2013-10-07 15:40:08 +0800

    fix some bugs

--- gcd.cpp
+++ gcd.cpp
@@ -10,12 +10,24 @@ int gcd(int a, int b){          //递归计算a和b的最大公因数
     }
 }
 
+int abs(int a){                 //对输入取绝对值
+    if(a < 0)
+        a = -a;
+    return a;
+}
+
 int main(){
     int a, b;
     while(1){
 	char temp;
         cout << "Please input a and b: " << endl;
         cin >> a >> b;
+        a = abs(a);
+        b = abs(b);
+        if(a == 0 && b == 0){  //当两个数都是0的时候求最大公因数无意义
+            cout << "Please input two Nonzero numbers." << endl;
+            continue;
+        }
         int ans = gcd(a, b);
         cout << "The gcd of a and b is : " << ans << endl;
 	cout << "if you want quit, type 'q', type 'c' to continue" << endl;
